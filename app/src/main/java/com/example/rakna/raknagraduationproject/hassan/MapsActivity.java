99948792184446cd37d618f16fce901e1292511b
hassan <hassanelkhadrawy59@gmail.com>
2019-01-25 18:43:43 +0200

    hassan

--- app/src/main/java/com/example/rakna/raknagraduationproject/hassan/MapsActivity.java
+++ app/src/main/java/com/example/rakna/raknagraduationproject/hassan/MapsActivity.java
@@ -40,7 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(27.19773, 31.16774);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
