378a8c34db07813d668b7d5ecc3cd038dbb4d34c
Azer <azer.nasr18@gmail.com>
2019-02-03 13:18:10 +0200

    first

---
+++ app/src/main/java/com/example/rakna/raknagraduationproject/Model/AzerModel/RegestrationModel.java
@@ -0,0 +1,8 @@
+package com.example.rakna.raknagraduationproject.Model.AzerModel;
+
+/**
+ * Created by Azer on 1/30/2019.
+ */
+
+public class RegestrationModel {
+}
---
+++ app/src/main/java/com/example/rakna/raknagraduationproject/Presenter/AzerPresenter/RegestrationPresenter.java
@@ -0,0 +1,8 @@
+package com.example.rakna.raknagraduationproject.Presenter.AzerPresenter;
+
+/**
+ * Created by Azer on 1/30/2019.
+ */
+
+public class RegestrationPresenter {
+}
